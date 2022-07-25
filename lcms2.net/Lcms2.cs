01b5d693a33c5418d20c76567ac10d5334262ce5
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-07-25 14:28:39 -0500

    Add AdaptationState

--- lcms2.net/Lcms2.cs
+++ lcms2.net/Lcms2.cs
@@ -6,6 +6,7 @@ public static class Lcms2
     public const int Version = 2131;
 
     public const int MaxPath = 256;
+    public const int MaxChannels = 16;
 
     public readonly static XYZ D50 = (0.9642, 1.0, 0.8249);
 
---
+++ lcms2.net/state/AdaptationState.cs
@@ -0,0 +1,21 @@
+﻿namespace lcms2.state;
+
+internal class AdaptationState
+{
+    private double adaptationState;
+
+    internal static void Alloc(ref Context ctx, in Context? src)
+    {
+        var from = src is not null ? (AdaptationState?)src.chunks[(int)Chunks.AdaptationStateContext] : adaptationStateChunk;
+
+        ctx.chunks[(int)Chunks.Logger] = from;
+    }
+
+    private AdaptationState(double value) =>
+        adaptationState = value;
+
+    internal static AdaptationState global = new(DefaultAdaptationState);
+    private readonly static AdaptationState adaptationStateChunk = new(DefaultAdaptationState);
+
+    internal static readonly double DefaultAdaptationState = 1.0;
+}
