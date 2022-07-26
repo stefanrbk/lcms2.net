46c1870da769ea491e76e7f4ccbea9fdb16029be
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-07-26 03:08:52 -0500

    Add IOHandler and NullStream

---
+++ lcms2.net/io/IOHandler.cs
@@ -0,0 +1,70 @@
+﻿using lcms2.state;
+
+using System;
+using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lcms2.io;
+public static class IOHandler
+{
+    public static ushort AdjustEndianness(ushort word) =>
+        BitConverter.IsLittleEndian ? BinaryPrimitives.ReverseEndianness(word) : word;
+    public static uint AdjustEndianness(uint dWord) =>
+        BitConverter.IsLittleEndian ? BinaryPrimitives.ReverseEndianness(dWord) : dWord;
+    public static ulong AdjustEndianness(ulong qWord) =>
+        BitConverter.IsLittleEndian ? BinaryPrimitives.ReverseEndianness(qWord) : qWord;
+    public static short AdjustEndianness(short word) =>
+        BitConverter.IsLittleEndian ? BinaryPrimitives.ReverseEndianness(word) : word;
+    public static int AdjustEndianness(int dWord) =>
+        BitConverter.IsLittleEndian ? BinaryPrimitives.ReverseEndianness(dWord) : dWord;
+    public static long AdjustEndianness(long qWord) =>
+        BitConverter.IsLittleEndian ? BinaryPrimitives.ReverseEndianness(qWord) : qWord;
+
+    public static byte? ReadUInt8Number(this Stream io)
+    {
+        var value = io.ReadByte();
+        return value is > byte.MaxValue or < byte.MinValue
+            ? null
+            : (byte)value;
+    }
+
+    public static ushort? ReadUInt16Number(this Stream io)
+    {
+        var tmp = new byte[2];
+        return io.Read(tmp, 0, 2) == 2
+            ? BinaryPrimitives.ReadUInt16BigEndian(tmp)
+            : null;
+    }
+
+    public static ushort[]? ReadUInt16Array(this Stream io, int count)
+    {
+        var tmp = new ushort[count];
+        for (var i = 0; i < count; i++)
+        {
+            var value = ReadUInt16Number(io);
+            if (value is null)
+                return null;
+            tmp[i] = (ushort)value;
+        }
+        return tmp;
+    }
+
+    public static uint? ReadUInt32Number(this Stream io)
+    {
+        var tmp = new byte[42];
+        return io.Read(tmp, 0, 4) == 4
+            ? BinaryPrimitives.ReadUInt32BigEndian(tmp)
+            : null;
+    }
+
+    public static ulong? ReadUInt64Number(this Stream io)
+    {
+        var tmp = new byte[8];
+        return io.Read(tmp, 0, 8) == 8
+            ? BinaryPrimitives.ReadUInt64BigEndian(tmp)
+            : null;
+    }
+}
---
+++ lcms2.net/io/NullStream.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lcms2.io;
+public class NullStream : Stream
+{
+    private long pointer;
+    private long length;
+
+    public override bool CanRead { get => true; }
+    public override bool CanSeek { get => true; }
+    public override bool CanWrite { get => true; }
+    public override long Length { get => length; }
+    public override long Position { get => pointer; set => pointer = value; }
+
+    public override void Flush() { }
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        length = count;
+        pointer += length;
+        return count;
+    }
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        switch (origin)
+        {
+            case SeekOrigin.Begin:
+                pointer = offset;
+                break;
+            case SeekOrigin.Current:
+                pointer += offset;
+                break;
+            case SeekOrigin.End:
+                pointer = length;
+                pointer -= offset;
+                break;
+        }
+        return pointer;
+    }
+    public override void SetLength(long value) =>
+        length = value;
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        pointer += count;
+        if (pointer > length)
+            length = pointer;
+    }
+}
