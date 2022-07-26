5ded92a3d0ce802eb65f9001f4de100a93ad87ed
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-07-26 17:02:13 -0500

    Add MutexPlugin

---
+++ lcms2.net/plugins/MutexPlugin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using lcms2.state;
+
+namespace lcms2.plugins;
+
+public delegate object? CreateMutexFunction(ref Context context);
+public delegate void DestroyMutexFunction(ref Context context, ref object mtx);
+public delegate bool LockMutexFunction(ref Context context, ref object mtx);
+public delegate void UnlockMutexFunction(ref Context context, ref object mtx);
+
+public class MutexPlugin
+{
+
+}
