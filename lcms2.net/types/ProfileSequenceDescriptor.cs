bd2ff6ecd8ada6d5224035a4ccc769d7461d33c2
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-07-27 01:22:51 -0500

    Add Sequence and related types

---
+++ lcms2.net/types/ProfileSequenceDescriptor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lcms2.types;
+public class ProfileSequenceDescriptor
+{
+    public Signature DeviceMfg;
+    public Signature DeviceModel;
+    public ulong Attributes;
+    public Signature Technology;
+    public ProfileID ProfileID;
+    public Mlu Manufacturer;
+    public Mlu Model;
+    public Mlu Description;
+}
---
+++ lcms2.net/types/Sequence.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using lcms2.state;
+
+namespace lcms2.types;
+public class Sequence
+{
+    public int Count;
+    public Context Context;
+    public ProfileSequenceDescriptor[] Seq;
+}
