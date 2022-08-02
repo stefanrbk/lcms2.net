c31685a27149b79bf990b35785efe1759780b127
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-08-02 13:00:15 -0500

    Add Register and UnregisterAll functions

--- lcms2.net/plugins/Plugin.cs
+++ lcms2.net/plugins/Plugin.cs
@@ -1,4 +1,6 @@
-﻿using lcms2.types;
+﻿using lcms2.state;
+using lcms2.state.chunks;
+using lcms2.types;
 
 namespace lcms2.plugins;
 
@@ -14,5 +16,92 @@ public abstract class Plugin
         Magic = magic;
         ExpectedVersion = expectedVersion;
         Type = type;
+    }
+
+    public static bool Register(Plugin? plugin) =>
+        Register(null, plugin);
+    
+    public static bool Register(Context? context, Plugin? plug_in)
+    {
+        for (var plugin = plug_in; plugin is not null; plugin = plugin.Next)
+        {
+            if (plugin.Magic != Signature.Plugin.MagicNumber)
+            {
+                Context.SignalError(context, ErrorCode.UnknownExtension, "Unrecognized plugin");
+                return false;
+            }
+
+            if (plugin.ExpectedVersion > Lcms2.Version)
+            {
+                Context.SignalError(context, ErrorCode.UnknownExtension,
+                    "plugin needs Little CMS {0}, current version is {1}", plugin.ExpectedVersion, Lcms2.Version);
+                return false;
+            }
+
+            if (plugin.Type == Signature.Plugin.Interpolation)
+            {
+                return PluginInterpolation.RegisterPlugin(context, plugin as PluginInterpolation);
+            }
+            else if (plugin.Type == Signature.Plugin.TagType)
+            {
+                return PluginTagType.RegisterPlugin(context, plugin as PluginTagType);
+            }
+            else if (plugin.Type == Signature.Plugin.Tag)
+            {
+                return PluginTag.RegisterPlugin(context, plugin as PluginTag);
+            }
+            else if (plugin.Type == Signature.Plugin.Formatters)
+            {
+                return PluginFormatter.RegisterPlugin(context, plugin as PluginFormatter);
+            }
+            else if (plugin.Type == Signature.Plugin.RenderingIntent)
+            {
+                return PluginRenderingIntent.RegisterPlugin(context, plugin as PluginRenderingIntent);
+            }
+            else if (plugin.Type == Signature.Plugin.ParametricCurve)
+            {
+                return PluginParametricCurves.RegisterPlugin(context, plugin as PluginParametricCurves);
+            }
+            else if (plugin.Type == Signature.Plugin.MultiProcessElement)
+            {
+                return PluginMultiProcessElement.RegisterPlugin(context, plugin as PluginMultiProcessElement);
+            }
+            else if (plugin.Type == Signature.Plugin.Optimization)
+            {
+                return PluginOptimization.RegisterPlugin(context, plugin as PluginOptimization);
+            }
+            else if (plugin.Type == Signature.Plugin.Translform)
+            {
+                return PluginTransform.RegisterPlugin(context, plugin as PluginTransform);
+            }
+            else if (plugin.Type == Signature.Plugin.Mutex)
+            {
+                return PluginMutex.RegisterPlugin(context, plugin as PluginMutex);
+            }
+            else
+            {
+                Context.SignalError(context, ErrorCode.UnknownExtension, "Unrecognized plugin type {0:X8}", plugin.Type);
+                return false;
+            }
+        }
+        // plug_in was null somehow? I would expect this to be false, but it is true in the original...
+        return true;
+    }
+
+    public static void UnregisterAll() =>
+        UnregisterAll(null);
+
+    public static void UnregisterAll(Context? context)
+    {
+        PluginInterpolation.RegisterPlugin(context, null);
+        PluginTagType.RegisterPlugin(context, null);
+        PluginTag.RegisterPlugin(context, null);
+        PluginFormatter.RegisterPlugin(context, null);
+        PluginRenderingIntent.RegisterPlugin(context, null);
+        PluginParametricCurves.RegisterPlugin(context, null);
+        PluginMultiProcessElement.RegisterPlugin(context, null);
+        PluginOptimization.RegisterPlugin(context, null);
+        PluginTransform.RegisterPlugin(context, null);
+        PluginMutex.RegisterPlugin(context, null);
     }
 }
