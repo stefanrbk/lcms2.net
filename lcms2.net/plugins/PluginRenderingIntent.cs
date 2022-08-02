6e3437df535cc7a59a2e452abcc9ce8cc89d85ba
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-08-01 21:24:46 -0500

    Add PluginRenderingIntent and related types into its file.

---
+++ lcms2.net/plugins/PluginRenderingIntent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using lcms2.state;
+using lcms2.types;
+
+namespace lcms2.plugins;
+
+public delegate Pipeline IntentFn(Context? context, int numProfiles, int[] intents, object[] profiles, bool[] bpc, double[] adaptationStates, uint flags);
+
+public sealed class PluginRenderingIntent : PluginBase
+{
+    public Signature Intent;
+    public IntentFn Link;
+    public string Description;
+
+    public PluginRenderingIntent(Signature magic, uint expectedVersion, Signature type, Signature intent, IntentFn link, string description)
+        : base(magic, expectedVersion, type)
+    {
+        Intent = intent;
+        Link = link;
+        Description = description;
+    }
+}
