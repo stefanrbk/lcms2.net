779874cb89b88f96df2c7c4d6084af7fde9e7df9
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-08-07 08:42:47 -0500

    Add docs for PluginParametricCurves

--- lcms2.net/plugins/PluginParametricCurves.cs
+++ lcms2.net/plugins/PluginParametricCurves.cs
@@ -3,19 +3,49 @@ using lcms2.types;
 
 namespace lcms2.plugins;
 
+/// <summary>
+///     Evaluator callback for user-supplied parametric curves. May implement more than one type
+/// </summary>
+/// <remarks>Implements the <c>cmsParametricCurveEvaluator</c> typedef.</remarks>
 public delegate double ParametricCurveEvaluator(Signature type, in double[] @params, double r);
+
+/// <summary>
+///     Parametric Curves
+/// </summary>
+/// <remarks>
+///     A plugin may implement an arbitrary number of parametric curves.<br />
+///     Implements the <c>cmsPluginParametricCurves</c> struct.
+/// </remarks>
 public sealed class PluginParametricCurves : Plugin
 {
+    /// <summary>
+    ///     Number of supported functions
+    /// </summary>
     public int NumFunctions;
+    /// <summary>
+    ///     The identification types
+    /// </summary>
+    /// <remarks>A negative type means same function but analytically inverted.</remarks>
     public int[] FunctionTypes;
+    /// <summary>
+    ///     The number of parameters for each function
+    /// </summary>
+    /// <remarks>Max of 10</remarks>
     public int[] ParameterCount;
 
-    public PluginParametricCurves(Signature magic, uint expectedVersion, Signature type, int numFunctions, int[] functionTypes, int[] parameterCount)
+    /// <summary>
+    ///     The evaluator
+    /// </summary>
+    public ParametricCurveEvaluator Evaluator;
+
+    public PluginParametricCurves(Signature magic, uint expectedVersion, Signature type, int numFunctions, int[] functionTypes, int[] parameterCount, ParametricCurveEvaluator evaluator)
         : base(magic, expectedVersion, type)
     {
         NumFunctions = numFunctions;
         FunctionTypes = functionTypes;
         ParameterCount = parameterCount;
+
+        Evaluator = evaluator;
     }
 
     internal static bool RegisterPlugin(Context? context, PluginParametricCurves? plugin)
