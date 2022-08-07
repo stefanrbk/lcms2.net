b9be6af238d0941b2be98f9ae6a577d010290686
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-08-07 16:49:27 -0500

    Add docs for PluginTransform

--- lcms2.net/plugins/PluginTransform.cs
+++ lcms2.net/plugins/PluginTransform.cs
@@ -5,11 +5,16 @@ using lcms2.types;
 
 namespace lcms2.plugins;
 
+/// <summary>
+///     Transform plugin
+/// </summary>
+/// <remarks>
+///     Implements the <c>cmsPluginTransform</c> typedef.</remarks>
 public sealed class PluginTransform : Plugin
 {
-    public TransformFactories Factories;
+    public Factory Factories;
 
-    public PluginTransform( Signature magic, uint expectedVersion, Signature type, TransformFactories factories)
+    public PluginTransform( Signature magic, uint expectedVersion, Signature type, Factory factories)
         : base(magic, expectedVersion, type) =>
         Factories = factories;
 
@@ -17,24 +22,29 @@ public sealed class PluginTransform : Plugin
     {
         throw new NotImplementedException();
     }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public unsafe struct Factory
+    {
+        [FieldOffset(0)]
+        public TransformFactory LegacyXform;
+
+        [FieldOffset(0)]
+        public Transform2Factory Xform;
+    }
 }
 
-[StructLayout(LayoutKind.Explicit)]
-public unsafe struct TransformFactories
+public unsafe struct Cache
 {
-    [FieldOffset(0)]
-    internal TransformFactory legacy_xform;
-
-    [FieldOffset(0)]
-    internal Transform2Factory xform;
+    public fixed ushort CacheIn[Lcms2.MaxChannels];
+    public fixed ushort CacheOut[Lcms2.MaxChannels];
 }
 
-public class Cache
-{
-    public ushort[] CacheIn = new ushort[Lcms2.MaxChannels];
-    public ushort[] CacheOut = new ushort[Lcms2.MaxChannels];
-}
-
+/// <summary>
+///     Stride info for a transform.
+/// </summary>
+/// <remarks>
+///     Implements the <c>cmsStride</c> struct.</remarks>
 public struct Stride
 {
     public int BytesPerLineIn;
@@ -43,12 +53,32 @@ public struct Stride
     public int BytesPerPlaneOut;
 }
 
+/// <summary>
+///     Legacy function, handles just ONE scanline.
+/// </summary>
+/// <remarks>
+///     Implements the <c>_cmsTransformFn</c> typedef.</remarks>
 public delegate void TransformFn(Transform cargo, object inputBuffer, object outputBuffer, int size, int stride);
 
+/// <summary>
+///     Transform function
+/// </summary>
+/// <remarks>
+///     Implements the <c>_cmsTransform2Fn</c> typedef.</remarks>
 public delegate void Transform2Fn(Transform cargo, object inputBuffer, object outputBuffer, int pixelsPerLine, int lineCount, Stride stride);
 
+/// <summary>
+///     Transform factory
+/// </summary>
+/// <remarks>
+///     Implements the <c>_cmsTransformFactory</c> typedef.</remarks>
 public delegate void TransformFactory(TransformFn xform, object? userData, object inputBuffer, object outputBuffer, int size, int stride);
 
+/// <summary>
+///     Transform factory
+/// </summary>
+/// <remarks>
+///     Implements the <c>_cmsTransform2Factory</c> typedef.</remarks>
 public delegate void Transform2Factory(Transform cargo, object inputBuffer, object outputBuffer, int pixelsPerLine, int lineCount, Stride stride);
 
 public class Transform
@@ -80,17 +110,43 @@ public class Transform
 
     internal Sequence Sequence;
 
-    internal uint OriginalFlags;
+    /// <summary>
+    ///     Retrieve original flags
+    /// </summary>
+    /// <remarks>
+    ///     Implements the <c>_cmsGetTransformFlags</c> and <c>_cmsGetTransformUserData</c> functions.</remarks>
+    public uint Flags { get; internal set; }
     internal double AdaptationState;
 
     internal Signature RenderingIntent;
 
     internal Context Context;
 
-    internal object? UserData;
+    /// <summary>
+    ///     User data as specified by the factory
+    /// </summary>
+    /// <remarks>
+    ///     Implements the <c>_cmsSetTransformUserData</c> and <c>_cmsGetTransformUserData</c> functions.</remarks>
+    public object? UserData { get; set; }
     internal FreeUserDataFn? FreeUserData;
 
     internal TransformFn? OldXform;
+
+    /// <summary>
+    ///     Retrieve 16 bit formatters
+    /// </summary>
+    /// <remarks>
+    ///     Implements the <c>_cmsGetTransformFormatters16</c> function.</remarks>
+    public (Formatter16? FromInput, Formatter16? ToOutput) Formatters16 =>
+        (FromInput, ToOutput);
+
+    /// <summary>
+    ///     Retrieve float formatters
+    /// </summary>
+    /// <remarks>
+    ///     Implements the <c>_cmsGetTransformFormattersFloat</c> function.</remarks>
+    public (FormatterFloat? FromInput, FormatterFloat? ToOutput) FormattersFloat =>
+        (FromInputFloat, ToOutputFloat);
 }
 public class TransformCollection
 {
