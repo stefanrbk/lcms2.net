0786689f97550a50df5fe08fc215fba032eb6773
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-08-07 17:21:51 -0500

    Add docs for PluginMutex

--- lcms2.net/plugins/PluginMutex.cs
+++ lcms2.net/plugins/PluginMutex.cs
@@ -3,17 +3,68 @@ using lcms2.types;
 
 namespace lcms2.plugins;
 
-public delegate object? CreateMutexFunction(ref Context context);
-public delegate void DestroyMutexFunction(ref Context context, ref object mtx);
-public delegate bool LockMutexFunction(ref Context context, ref object mtx);
-public delegate void UnlockMutexFunction(ref Context context, ref object mtx);
+/// <summary>
+///     Function to create a mutex
+/// </summary>
+/// <remarks>
+///     Implements the <c>_cmsCreateMutexFnPtrType</c> typedef.</remarks>
+public delegate object? CreateMutexFunction(Context? context);
 
+/// <summary>
+///     Function to destroy a mutex
+/// </summary>
+/// <remarks>
+///     Implements the <c>_cmsDestroyMutexFnPtrType</c> typedef.</remarks>
+public delegate void DestroyMutexFunction(Context? context, ref object mtx);
+
+/// <summary>
+///     Function to lock a mutex
+/// </summary>
+/// <remarks>
+///     Implements the <c>_cmsLockMutexFnPtrType</c> typedef.</remarks>
+public delegate bool LockMutexFunction(Context? context, ref object mtx);
+
+/// <summary>
+///     Function to unlock a mutex
+/// </summary>
+/// <remarks>
+///     Implements the <c>_cmsUnlockMutexFnPtrType</c> typedef.</remarks>
+public delegate void UnlockMutexFunction(Context? context, ref object mtx);
+
+/// <summary>
+///     Mutex plugin
+/// </summary>
+/// <remarks>
+///     Implements the <c>cmsPluginMutex</c> typedef.</remarks>
 public sealed class PluginMutex : Plugin
 {
-    public CreateMutexFunction CreateMutex;
-    public DestroyMutexFunction DestroyMutex;
-    public LockMutexFunction LockMutex;
-    public UnlockMutexFunction UnlockMutex;
+    /// <summary>
+    ///     Function to create a mutex
+    /// </summary>
+    /// <remarks>
+    ///     Implements the <c>_cmsCreateMutex</c> typedef.</remarks>
+    public CreateMutexFunction CreateMutex { get; internal set; }
+
+    /// <summary>
+    ///     Function to destroy a mutex
+    /// </summary>
+    /// <remarks>
+    ///     Implements the <c>_cmsDestroyMutex</c> typedef.</remarks>
+    public DestroyMutexFunction DestroyMutex { get; internal set; }
+
+    /// <summary>
+    ///     Function to lock a mutex
+    /// </summary>
+    /// <remarks>
+    ///     Implements the <c>_cmsLockMutex</c> typedef.</remarks>
+    public LockMutexFunction LockMutex { get; internal set; }
+
+    /// <summary>
+    ///     Function to unlock a mutex
+    /// </summary>
+    /// <remarks>
+    ///     Implements the <c>_cmsUnlockMutex</c> typedef.</remarks>
+    public UnlockMutexFunction UnlockMutex { get; internal set; }
 
     public PluginMutex(Signature magic, uint expectedVersion, Signature type, CreateMutexFunction create, DestroyMutexFunction destroy, LockMutexFunction @lock, UnlockMutexFunction unlock)
         : base(magic, expectedVersion, type)
