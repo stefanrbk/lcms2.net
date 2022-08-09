b5fb62ca2b318c00a977df6901d4def0fbe62d53
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-08-08 21:52:22 -0500

    Add ColorantOrderHandler

---
+++ lcms2.net/types/type_handlers/ColorantOrderHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using lcms2.io;
+using lcms2.plugins;
+using lcms2.state;
+
+namespace lcms2.types.type_handlers;
+public class ColorantOrderHandler : ITagTypeHandler
+{
+    public Signature Signature { get; }
+    public Context? Context { get; }
+    public uint ICCVersion => 0;
+
+    public object? Duplicate(ITagTypeHandler handler, object value, int num) =>
+        ((byte[])value).Clone();
+    public void Free(ITagTypeHandler handler, object value) { }
+    public object? Read(ITagTypeHandler handler, Stream io, int sizeOfTag, out int numItems)
+    {
+        numItems = 0;
+        if (!io.ReadUInt32Number(out var count)) return null;
+        if (count > Lcms2.MaxChannels) return null;
+
+        byte[] colorantOrder = new byte[Lcms2.MaxChannels];
+
+        // We use FF as end marker
+        for (var i = 0; i < Lcms2.MaxChannels; i++)
+            colorantOrder[i] = 0xFF;
+
+        if (io.Read(colorantOrder, 0, (int)count) != count) return null;
+
+        numItems = 1;
+        return colorantOrder;
+    }
+    public bool Write(ITagTypeHandler handler, Stream io, object value, int numItems)
+    {
+        var colorantOrder = (byte[])value;
+        int count;
+
+        // Get the length
+        for (var i = count = 0; i < Lcms2.MaxChannels; i++)
+            if (colorantOrder[i] != 0xFF) count++;
+
+        if (!io.Write(count)) return false;
+
+        var sz = count * sizeof(byte);
+        io.Write(colorantOrder, 0, sz);
+
+        return true;
+    }
+}
