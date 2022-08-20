a7d2c70bbd9afc08813700a1db60de2d9f2df8d2
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-08-20 15:06:12 -0500

    Add keywords

---
+++ lcms2.net/it8/Keyword.cs
@@ -0,0 +1,28 @@
+﻿namespace lcms2.it8;
+internal struct Keyword
+{
+    public string Id;
+    public Symbol Symbol;
+
+    public Keyword(string id, Symbol symbol) =>
+        (Id, Symbol) = (id, symbol);
+}
+
+internal static class Keywords
+{
+    public static readonly Keyword[] List = new Keyword[]
+    {
+        new("$INCLUDE", Symbol.Include),    // This is an extension!
+        new(".INCLUDE", Symbol.Include),    // This is an extension!
+
+        new("BEGIN_DATA", Symbol.BeginData),
+        new("BEGIN_DATA_FORMAT", Symbol.BeginDataFormat),
+        new("DATA_FORMAT_IDENTIFIER", Symbol.DataFormatId),
+        new("END_DATA", Symbol.SendData),
+        new("END_DATA_FORMAT", Symbol.SendDataFormat),
+        new("KEYWORD", Symbol.Keyword),
+    };
+
+    public static int Count =>
+        List.Length;
+}
