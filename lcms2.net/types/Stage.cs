aeed1d01145bf962040a946a47ad3089e11b81b1
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-08-31 14:01:46 -0500

    Remove CLut property

--- lcms2.net/types/Stage.cs
+++ lcms2.net/types/Stage.cs
@@ -31,9 +31,6 @@ public sealed partial class Stage: ICloneable, IDisposable
     internal ToneCurve[] CurveSet =>
         (Data as ToneCurveData)?.TheCurves ?? throw new InvalidOperationException();
 
-    internal CLutData CLut =>
-        (Data as CLutData) ?? throw new InvalidOperationException();
-
     public object Clone()
     {
         var result = AllocPlaceholder(
