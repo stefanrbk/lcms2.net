44148694f9f598e566ffa7409677cf231018d4c1
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-09-22 18:36:23 -0500

    Add WhitePoint tests

---
+++ UnitTests/WhitePointTests.cs
@@ -0,0 +1,48 @@
+﻿//---------------------------------------------------------------------------------
+//
+//  Little Color Management System
+//  Copyright (c) 1998-2022 Marti Maria Saguer
+//                2022      Stefan Kewatt
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software
+// is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//---------------------------------------------------------------------------------
+//
+namespace lcms2.tests;
+
+[TestFixture(TestOf = typeof(WhitePoint))]
+public class WhitePointTests
+{
+    #region Public Methods
+
+    [Test, Category(RandomTest)]
+    public void CheckTemp2Chroma(
+        [Random(4000, 25000, 20, Distinct = true)]
+            int j)
+    {
+        var white = WhitePoint.FromTemp(j);
+        var v = WhitePoint.ToTemp(white);
+
+        Assert.That(v, Is.Not.NaN);
+
+        Assert.That(v, Is.EqualTo(j).Within(100));
+    }
+
+    #endregion Public Methods
+}
