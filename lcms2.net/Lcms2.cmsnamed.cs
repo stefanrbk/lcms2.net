530a928ac43b8cfc013f374b351389571e831572
Stefan Kewatt <stefan.kewatt@gmail.com>
2023-10-15 03:29:03 -0500

    Add changes from cmsnamed.c

--- lcms2.net/Lcms2.cmsnamed.cs
+++ lcms2.net/Lcms2.cmsnamed.cs
@@ -387,9 +387,12 @@ public static partial class Lcms2
         //if (len is not null) *len = v->Len;
 
         //return (char*)((byte*)mlu->MemPool + v->StrWCharOffset);
+        if (v.StrWOffsetInBytes + v.LenInBytes > mlu.PoolSizeInBytes)
+            return null;
+
         var result = mlu.MemPool.AsSpan();
 
-        return TrimBuffer(mlu.MemPool.AsSpan()[((int)v.StrWOffsetInBytes / sizeof(char))..][..((int)v.LenInBytes / sizeof(char))]);
+        return TrimBuffer(result[((int)v.StrWOffsetInBytes / sizeof(char))..][..((int)v.LenInBytes / sizeof(char))]);
     }
 
     public static uint cmsMLUgetASCII(
