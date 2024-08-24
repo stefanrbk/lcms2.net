544a0c21b0d3e0a63370a426b1db06db1caab1c3
Stefan Kewatt <stefan.kewatt@gmail.com>
2024-08-24 00:33:56 -0500

    Add first test for 15 bit conversions

--- plugins/lcms2_fast_float_plugin_unit_tests/_15BitTests.cs
+++ plugins/lcms2_fast_float_plugin_unit_tests/_15BitTests.cs
@@ -1,8 +1,17 @@
-﻿using lcms2.types;
+﻿using lcms2.state;
+using lcms2.types;
+
+using System.Resources;
 
 namespace lcms2.FastFloatPlugin.tests;
 public class _15BitTests
 {
+    private readonly Context _ctx = cmsCreateContext()!;
+
+    [SetUp]
+    public void Setup() =>
+        cmsPluginTHR(_ctx, cmsFastFloatExtensions());
+
     [TestCaseSource(nameof(Test15BitFormattersReturnInputsAfterRoundTripCases))]
     public void Test15BitFormattersReturnInputsAfterRoundTrip(string a)
     {
@@ -58,6 +67,76 @@ public class _15BitTests
                 Assert.That(m, Is.EqualTo(i));
             }
         });
+    }
+
+    [TestCase(nameof(TestProfiles.test5), nameof(TestProfiles.test3), INTENT_PERCEPTUAL, Description = nameof(INTENT_PERCEPTUAL))]
+    public void TestConversionsOnAll15BitValues(string profileInName, string profileOutName, int Intent)
+    {
+        var resources = new ResourceManager("lcms2.FastFloatPlugin.tests.TestProfiles", typeof(_15BitTests).Assembly);
+
+        var profileInData = (byte[])resources.GetObject(profileInName)!;
+        var profileOutData = (byte[])resources.GetObject(profileOutName)!;
+
+        var profileIn = cmsOpenProfileFromMemTHR(_ctx, profileInData)!;
+        var profileOut = cmsOpenProfileFromMemTHR(_ctx, profileOutData)!;
+
+        var xform15 = cmsCreateTransformTHR(_ctx, profileIn, TYPE_RGB_15, profileOut, TYPE_RGB_15, (uint)Intent, cmsFLAGS_NOCACHE);
+        var xform8 = cmsCreateTransformTHR(_ctx, profileIn, TYPE_RGB_8, profileOut, TYPE_RGB_8, (uint)Intent, cmsFLAGS_NOCACHE);
+
+        cmsCloseProfile(profileIn);
+        cmsCloseProfile(profileOut);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(xform15, Is.Not.Null);
+            Assert.That(xform8, Is.Not.Null);
+        });
+
+        const int npixels = 256 * 256 * 256;  // All RGB cube in 8 bits
+
+        var buffer8in = new Scanline_rgb8bits[npixels];
+        var buffer8out = new Scanline_rgb8bits[npixels];
+        var buffer15in = new Scanline_rgb15bits[npixels];
+        var buffer15out = new Scanline_rgb15bits[npixels];
+
+        // Fill input values for 8 and 15 bits
+        var j = 0;
+        for (var r = 0; r < 256; r++)
+        {
+            for (var g = 0; g < 256; g++)
+            {
+                for (var b = 0; b < 256; b++)
+                {
+                    buffer8in[j].r = (byte)r;
+                    buffer8in[j].g = (byte)g;
+                    buffer8in[j].b = (byte)b;
+
+                    buffer15in[j].r = FROM_8_TO_15((byte)r);
+                    buffer15in[j].g = FROM_8_TO_15((byte)g);
+                    buffer15in[j].b = FROM_8_TO_15((byte)b);
+
+                    j++;
+                }
+            }
+        }
+
+        cmsDoTransform(xform15, buffer15in, buffer15out, npixels);
+        cmsDoTransform(xform8, buffer8in, buffer8out, npixels);
+
+        Assert.Multiple(() =>
+        {
+            for (var j = 0; j < npixels; j++)
+            {
+                // Check the results
+                Assert.Multiple(() =>
+                {
+                    Assert.That(FROM_15_TO_8(buffer15out[j].r), Is.EqualTo(buffer8out[j].r).Within(2));
+                    Assert.That(FROM_15_TO_8(buffer15out[j].g), Is.EqualTo(buffer8out[j].g).Within(2));
+                    Assert.That(FROM_15_TO_8(buffer15out[j].b), Is.EqualTo(buffer8out[j].b).Within(2));
+                });
+            }
+        });
+
     }
 
     internal static object[] Test15BitFormattersReturnInputsAfterRoundTripCases =
