551b0ad2d99574874cd4b521a2aa9b99393f1c5e
Stefan Kewatt <stefan.kewatt@gmail.com>
2024-08-25 22:09:04 -0500

    Add test for transform creation failure when requesting a transform with mismatched channels and requesting alpha channel copy

---
+++ plugins/lcms2_fast_float_plugin_unit_tests/FloatConversionTests.cs
@@ -0,0 +1,32 @@
+﻿using lcms2.state;
+
+namespace lcms2.FastFloatPlugin.tests;
+public class FloatConversionTests
+{
+    private static readonly Context _pluginCtx = cmsCreateContext()!;
+    private static readonly Context _rawCtx = cmsCreateContext()!;
+
+    [OneTimeSetUp]
+    public void Setup() =>
+        cmsPluginTHR(_pluginCtx, cmsFastFloatExtensions());
+
+    [OneTimeTearDown]
+    public void Cleanup()
+    {
+        cmsDeleteContext(_rawCtx);
+        cmsDeleteContext(_pluginCtx);
+    }
+
+    [Test]
+    public void TestTransformCreationFailureWhenUsingMismatchedChannelsAndCopyAlpha()
+    {
+        cmsSetLogErrorHandlerTHR(_pluginCtx, BuildNullLogger());
+
+        var hsRGB = cmsCreate_sRGBProfileTHR(_pluginCtx)!;
+
+        var xform = cmsCreateTransformTHR(_pluginCtx, hsRGB, TYPE_RGB_FLT, hsRGB, TYPE_RGBA_FLT, INTENT_PERCEPTUAL, cmsFLAGS_COPY_ALPHA);
+        cmsCloseProfile(hsRGB);
+
+        Assert.That(xform, Is.Null);
+    }
+}
